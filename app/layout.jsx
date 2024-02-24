f5c3939c170bc9940ea273dbefc552b43d6a3a88
Siddhant Trivedi <siddhanttrivedi56@gmail.com>
2024-02-24 22:39:20 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "Property Pulse | Find the perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental,find rentals,find properties",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
